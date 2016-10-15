ffc01800fbac92aed36b656f14fb8b6a3f4177fe
Lukas Blaho <lukas.blaho@gmail.com>
2016-10-15 20:31:59 +0200

    userForm migrate v5 -> v6 redux form

--- app/components/User/include/UserForm.js
+++ app/components/User/include/UserForm.js
@@ -1,38 +1,37 @@
 import React, {Component} from 'react'
-import {reduxForm} from 'redux-form'
+import {reduxForm, Field} from 'redux-form'
+
+const renderInput = field =>
+    <div className="col-sm-10">
+        <input className="form-control" type={field.type} placeholder="Last Name" {...field.input}/>
+        {field.meta.touched && field.meta.error && <span>{field.meta.error}</span>}
+    </div>
+
+const renderInputHidden = field =>
+        <input className="form-control" type={field.type} placeholder="Last Name" {...field}/>
 
 const UserFormTpl = (props) => {
-    const {
-        fields: {id, firstName, lastName, email},
-        handleSubmit,
-        submitting
-    } = props
+    const {handleSubmit, submitting} = props
 
     return (
         <div className="row">
             <div className="col-md-12">
                 <form onSubmit={handleSubmit} className="form-horizontal">
-                    <input className="form-control" type="hidden" {...id}/>
+                    <Field name="id" component={renderInputHidden} type="hidden" />
 
                     <div className="form-group">
                         <label className="col-sm-2 control-label">First Name</label>
-                        <div className="col-sm-10">
-                            <input className="form-control" type="text" placeholder="First Name" {...firstName}/>
-                        </div>
-                        {firstName.touched && firstName.error && <span>{firstName.error}</span>}
+                        <Field name="firstName" component={renderInput} type="text" />
                     </div>
                     <div className="form-group">
                         <label className="col-sm-2 control-label">Last Name</label>
-                        <div className="col-sm-10">
-                            <input className="form-control" type="text" placeholder="Last Name" {...lastName}/>
-                        </div>
+                        <Field name="lastName" component={renderInput} type="text" />
                     </div>
                     <div className="form-group">
                         <label className="col-sm-2 control-label">Email</label>
-                        <div className="col-sm-10">
-                            <input className="form-control" type="email" placeholder="Email" {...email}/>
-                        </div>
+                        <Field name="email" component={renderInput} type="text" />
                     </div>
+
                     <div className="form-group">
                         <div className="col-md-offset-2 col-md-10">
                             <button className="btn btn-default" type="submit" disabled={submitting}>Submit</button>
@@ -44,20 +43,7 @@ const UserFormTpl = (props) => {
     );
 }
 
-const asyncValidate = values => {
-    return fetch()
-        .then(status)
-        .then(json => {
-            if (json.isError) {
-                throw json.errors;
-            }
-        })
-        .catch(error => {
-            throw {text: 'Error on validating form'}
-        })
-}
-
-const validate = values => {
+const validate = values => { console.log(values)
     const errors = {}
 
     if (!values.firstName) {
@@ -67,11 +53,7 @@ const validate = values => {
     return errors
 }
 
-const UserForm = reduxForm({
-    form: 'user',
-    fields: ['id', 'firstName', 'lastName', 'email'],
-    validate,
-    //asyncValidate,
+export default reduxForm({
+    form: 'userForm',
+    validate
 })(UserFormTpl);
-
-export default UserForm;
